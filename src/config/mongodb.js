1ec38a8b4db241b5020d7ec717427f5bff5531cf
Quach Bui Song Dang <quachbuisongdang@gmail.com>
2024-10-30 19:59:46 +0700

    routes

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,18 +1,16 @@
-const MONGODB_URI = 'mongodb+srv://quachbuitoankhoa:lttq12345@cluster0.2dljt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-const DATABASE_NAME = 'LTTQ'
 
 import { MongoClient, ServerApiVersion } from 'mongodb'
-
+import { env } from '~/config/environment'
 //Khoi tao la null do chua connect
 let trelloDatabaseInstance = null
 
 //Khoi tao doi tuong client connect toi mongodb
-const mongoClientInstance = new MongoClient(MONGODB_URI, {
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true
-  } 
+  }
 })
 
 //Ket noi toi database
@@ -21,7 +19,7 @@ export const CONNECT_DB = async () => {
   await mongoClientInstance.connect()
 
   //Neu ket noi thanh cong thi lay ra database theo ten va gan nguoc lai vao bien trellodatabaseinstance
-  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
 }
 
 //Lay ra databaseinstance sau khi da ket noi thanh cong de dung o nhieu noi khac trong code, chi lay duoc neu da ket noi thanh cong
